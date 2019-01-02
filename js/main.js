722a9f3968701182f085b3ce0d8d593ec9068685
soonoo <fetra09@naver.com>
2019-01-02 14:00:13 +0900

    주석 추가

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ $(function () {
         // 최초 메뉴 클릭 시 메뉴가 열리고 그 후 부터는 화면 안에 내용이 전환 된다.
         if (menuSwitch == false) {
             menuSwitch = true;
+            // 헤더 부분의 스타일이 변경 되며, 모바일에서는 다르게 작동한다.
             $('.nav-menu .btn-on, .nav-menu .btn-on a, .nav-menu .lang li a').stop().animate({color: '#000'}, 1500, function () {
                 $('.nav-menu').css('background', '#fff');
                 $('.menu-bar').css('color', '#000');
@@ -26,6 +27,7 @@ $(function () {
             $('#main-page-circle').css('display', 'none');
             $('#' + id + '-circle').css('display', 'flex');
             $('.content').css('display', 'block');
+            // 컨텐츠의 컬러는 각 메뉴에 data-target의 값에 따라 결정 된다.
             $('.color').css('color', $(this).attr('data-target'));
             $('.color-line').css('background', $(this).attr('data-target'));
             $('#' + id + '-content').fadeIn();
@@ -36,6 +38,7 @@ $(function () {
                 $('#menu-off').addClass('dis-none');
             }
         } else {
+            // 이미 화면이 띄워져 있다면 안에 있던 컨텐츠만 변경 되며, 이 때도 data-target의 컬러값을 따라간다.
             $('html, body').animate({scrollTop: 0}, 400);
             $('.color').css('color', $(this).attr('data-target'));
             $('.color-line').css('background', $(this).attr('data-target'));
@@ -55,6 +58,8 @@ $(function () {
             }
         }
     });
+
+    // 마우스 오버시 데스트탑과 모바일 버전 별도의 메뉴 스타일 변경
     $('.nav-menu .btn-on').mouseover(function () {
         var color = $(this).attr('data-target');
         if (window.innerWidth >= 1178) {
@@ -75,6 +80,8 @@ $(function () {
             $(this).stop().css('background', '#000');
         }
     });
+
+    // 모바일 메뉴바 클릭시 메뉴 스타일 변경
     $('.menu-bar').click(function () {
         if (window.innerWidth < 1178) {
             $('.nav-menu').stop().slideToggle();
@@ -107,6 +114,8 @@ $(function () {
 });
 
 $(function () {
+
+    // 메인 화면의 텍스트 사이즈 width와 height에 따라 변동된다.
     var autoFont = function () {
         if(window.innerWidth > 1178) {
             $(".main-title").css('font-size', Math.max(Math.min($("body").height() * 4 + Math.min($("body").width())) / (3.3 * 10)));
